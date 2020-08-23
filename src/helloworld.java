24f2a1e4663d5db6660ed93acbdfb65930e3686d
Hamza-1999 <68568463+Hamza-1999@users.noreply.github.com>
2020-08-23 14:31:52 +0200

    Initial commit

---
+++ src/helloworld.java
@@ -0,0 +1,10 @@
+public class helloworld {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+
+        System.out.println("Teller til 10");
+        for (int i=0;i<10;i++){
+            System.out.println(i);
+        }
+    }
+}
